f5cca59a413b29802251a2f527648e793e6078e7
yangyx2025 <yangyuexin2025@163.com>
2025-04-17 20:31:57 +0800

    switch 转为字典索引增加容错，优化了数据读取函数和代码逻辑

--- pipeline03_read_neuron_res.m
+++ pipeline03_read_neuron_res.m
@@ -1,48 +1,43 @@
 %读取cnmf-e结果
 % yyx20250413 v1.0.0
+%yyx20250417 v1.1.0 switch 转为字典索引增加容错，优化了数据读取函数和代码逻辑
 clear;
 clc;
 close all;
-filepath='K:\m0728\20240828_sleep_sti';
+filepath='K:\m0728\20240828_sleep_sti\res';
 %% 
-filename='export.hdf5';
 savename='neuron_align01.mat';
-savepath=filepath;
-member_num=7;
-n=0;
-txt_name=fullfile(filepath,'export_list.txt');
-fullname=fullfile(filepath,filename);
-exportlist=FunReadHdf5Txt(txt_name);
-for i=1:length(exportlist)
-    if strcmp(exportlist{i,2},'unselected_good')
-        if ~isempty(exportlist{i,3})
-            hdf5_name=['/',exportlist{i,1},'/',exportlist{i,2},'/',exportlist{i,3}];
-            n=n+1;
-            patch_id=ceil(n/member_num);
-            member_id=mod(n,member_num);
-            neuron_patch(patch_id).id=exportlist{i,1};
-            switch member_id
-                case 1
-                    neuron_patch(patch_id).center=h5read(fullname,hdf5_name);
-                case 2
-                    neuron_patch(patch_id).trace=h5read(fullname,hdf5_name);
-                case 3
-                    neuron_patch(patch_id).p_cnn=h5read(fullname,hdf5_name);
-                case 4
-                    neuron_patch(patch_id).p_corr=h5read(fullname,hdf5_name);
-                case 5
-                    neuron_patch(patch_id).patch_id=h5read(fullname,hdf5_name);
-                case 6
-                    neuron_patch(patch_id).spike=h5read(fullname,hdf5_name);
-                case 0
-                    neuron_patch(patch_id).p_snr=h5read(fullname,hdf5_name);
-            end
-        end
-    end
+exportlist_txt=fullfile(filepath,'export_list.txt');
+hdf5file=fullfile(filepath,'export.hdf5');
+%读取文件目录,生成patch的索引
+[hdf5_filelist,patch_id]=FunReadHdf5Txt(exportlist_txt);
 
+%% 依次读取目录内容并整合
+for i=1:length(hdf5_filelist)
+    %依次读取
+    hdf5_name=['/',hdf5_filelist{i,1},'/',hdf5_filelist{i,2},'/',hdf5_filelist{i,3}];
+    patch_id_buff=patch_id(i)+1;%python 与matlab index 转换
+    sub_name=hdf5_filelist{i,3};
+    neuron_patch(patch_id_buff).patch_id=patch_id(i);
+    switch sub_name
+        case 'good_a'
+            neuron_patch(patch_id_buff).center=h5read(hdf5file,hdf5_name);%neuron center
+        case 'good_c'
+            neuron_patch(patch_id_buff).trace=h5read(hdf5file,hdf5_name);%neuron trace
+        case 'good_cnn'
+            neuron_patch(patch_id_buff).p_cnn=h5read(hdf5file,hdf5_name);
+        case 'good_corr'
+            neuron_patch(patch_id_buff).p_corr=h5read(hdf5file,hdf5_name);
+        case 'good_patchid'
+            neuron_patch(patch_id_buff).neuron_id=h5read(hdf5file,hdf5_name);%neuron_id in one patch
+        case 'good_s'
+            neuron_patch(patch_id_buff).spike=h5read(hdf5file,hdf5_name);
+        case 'good_snr'
+            neuron_patch(patch_id_buff).p_snr=h5read(hdf5file,hdf5_name);
+    end
 end
 
-%% intergration
+%% intergration整合到neuron_align
 neuron_align.neuron.trace=[]; 
 neuron_align.neuron.location=[];
 neuron_align.neuron.spike=[];
@@ -51,18 +46,18 @@ neuron_align.neuron.patch_id=[];
 neuron_align.neuron.p_corr=[];
 neuron_align.neuron.p_cnn=[];
 neuron_align.neuron.patch=[];
-neuron_align.neuron.para=h5read(fullname,'/param');
-for i=1:patch_id
+neuron_align.neuron.para=h5read(hdf5file,'/param');
+
+for i=1:size(neuron_patch,2)
     location_buff=neuron_patch(i).center;
     trace_buff=neuron_patch(i).trace;
     spike_buff=neuron_patch(i).spike;
-    patch_id_buff=neuron_patch(i).patch_id;
+    patch_id_buff=neuron_patch(i).neuron_id;
     p_snr_buff=neuron_patch(i).p_snr;
     p_corr_buff=neuron_patch(i).p_corr;
     p_cnn_buff=neuron_patch(i).p_cnn;
-    num=length(p_cnn_buff);
-    patch_num=str2double(neuron_patch(i).id(isstrprop(neuron_patch(i).id,'digit')));
-    patch_buff=ones(num,1)*patch_num;
+    patch_buff=ones(length(p_cnn_buff),1)*neuron_patch(i).patch_id;
+
     neuron_align.neuron.trace=[neuron_align.neuron.trace;trace_buff'];
     neuron_align.neuron.location=[neuron_align.neuron.location;location_buff];
     neuron_align.neuron.spike=[neuron_align.neuron.spike;spike_buff'];
@@ -73,36 +68,22 @@ for i=1:patch_id
     neuron_align.neuron.patch=[neuron_align.neuron.patch;patch_buff];
 end
 %% 
-
-save(fullfile(savepath,savename2),'neuron_align','-v7.3');
+save(fullfile(filepath,savename),'neuron_align','-v7.3');
+%% 
 % h5disp(fullname)
 % h5read(fullname,'/patch1/unselected_good');
+%% 
+function [res_cell_string,patch_ids] = FunReadHdf5Txt(txt_name)
+    %读取hdf5的导出文件目录文本
+    % 逐行读取、以 '/' 分割，再转为 cell 数组
+    res_cell=readcell(txt_name, 'Delimiter', '/', 'TextType','string');
+    res_cell_string=string(res_cell);
+    res_cell_string(ismissing(res_cell_string))="";
 
-
-function exportlist=FunReadHdf5Txt(txt_name)
-    opts = delimitedTextImportOptions("NumVariables", 3);
-
-% 指定范围和分隔符
-opts.DataLines = [1, Inf];
-opts.Delimiter = "/";
-
-% 指定列名称和类型
-opts.VariableNames = ["param", "VarName2", "VarName3"];
-opts.VariableTypes = ["char", "char", "char"];
-
-% 指定文件级属性
-opts.ExtraColumnsRule = "ignore";
-opts.EmptyLineRule = "read";
-
-% 指定变量属性
-opts = setvaropts(opts, ["param", "VarName2", "VarName3"], "WhitespaceRule", "preserve");
-opts = setvaropts(opts, ["param", "VarName2", "VarName3"], "EmptyFieldRule", "auto");
-
-% 导入数据
-exportlist = readtable(txt_name, opts);
-exportlist = table2cell(exportlist);
-numIdx = cellfun(@(x) ~isnan(str2double(x)), exportlist);
-exportlist(numIdx) = cellfun(@(x) {str2double(x)}, exportlist(numIdx));
-clear opts
-
+    %去掉无关部分
+    mask= strcmp(res_cell_string(:,2), 'unselected_good') & ~cellfun(@isempty, res_cell_string(:,3));
+    res_cell_string = res_cell_string(mask, :);
+    %读取patch id
+    patch_ids   = cellfun(@(s) sscanf(s,'patch%d'), res_cell_string(:,1));
+    
 end
