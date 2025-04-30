515d8a805b65f8b29eaa8e17e11e64866af9abfa
yangyx2025 <yangyuexin2025@163.com>
2025-04-30 14:44:22 +0800

    Add files via upload

--- pipeline03_read_neuron_res.m
+++ pipeline03_read_neuron_res.m
@@ -5,7 +5,7 @@
 clear;
 clc;
 close all;
-filepath='K:\m0728\20240828_sleep_sti\res';
+filepath='K:\m0728\20240830_sleep_sti\res';
 %% 
 exportlist_txt=fullfile(filepath,'export_list.txt');
 hdf5file=fullfile(filepath,'export.hdf5');
@@ -76,7 +76,7 @@ for i=1:size(neuron_patch,1)
     neuron_align.neuron.patch=[neuron_align.neuron.patch;patch_buff];
 end
 %% 
-save(fullfile(filepath,'neuron_align.mat'),'neuron_align','-v7.3');
+save(fullfile(filepath,'neuron_align01.mat'),'neuron_align','-v7.3');
 %% 
 % h5disp(fullname)
 % h5read(fullname,'/patch1/unselected_good');
